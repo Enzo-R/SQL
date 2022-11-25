9e2ea6ada6668d54b0850a320b1b7ed0884f66e1
root <root@Enzo-ThinkPad.localdomain>
2022-11-25 18:29:14 -0300

    Arquivo para alterações no banco

---
+++ Alterações.sql
@@ -0,0 +1,15 @@
+--email da tbl_alunos para UNIQUE
+--TRUNCATE TABLE para apagar os dados da tabela
+
+USE teste
+ALTER TABLE table_Emprestimos
+ADD Id_Aluno INT FOREIGN KEY REFERENCES table_Alunos(Ra)
+
+USE teste
+ALTER TABLE table_Emprestimos
+ADD Id_livro INT FOREIGN KEY REFERENCES table_Livros(LivrosId)
+
+USE teste
+ALTER TABLE table_Alunos
+ADD CONSTRAINT AK_Email
+UNIQUE (Email)
